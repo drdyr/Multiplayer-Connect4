c17131be636b2ebd6aaab679bd7d26e1ebd5d0bb
seb <seb.hallx@gmail.com>
2020-06-27 15:58:07 +0100

    added turn indicator and game ends when player wins

--- connect4 v2.py
+++ connect4 v2.py
@@ -14,11 +14,14 @@ class Board:
             self.entry_slots.append(EntrySlot(i))
             for j in range(6):
                 self.slots.append(Slot(i, j))
+        self.won = False
 
     def add_disk(self, disk):
         disk.update()
         self.disks.append(disk)
-        print(self.check_win(disk))
+        if self.check_win(disk):
+            self.won = True
+
 
     def draw(self):
         for slot in self.slots:
@@ -107,9 +110,6 @@ class Disk:
         while self.y < 5 and Disk(self.x, self.y + 1, red) not in board.disks:
             self.y += 1
 
-
-
-
 white = (255, 255, 255)
 yellow = (255, 255, 0)
 red = (255, 0, 0)
@@ -118,12 +118,18 @@ grey = (150, 150, 150)
 black = (0, 0, 0)
 green = (0, 255, 0)
 
-screen = pygame.display.set_mode((700, 620))
+screen = pygame.display.set_mode((700, 720))
 screen.fill(blue)
+bottom_text_panel = (0, 620, 700, 100)
 
 turn = 1
 
 board = Board()
+font = pygame.font.SysFont('Comic Sans', 50)
+display_red_turn = font.render("It is red's turn.", False, black)
+display_yellow_turn = font.render("It is yellow's turn.", False, black)
+display_red_won = font.render("Player red won!", False, black)
+display_yellow_won = font.render("Player yellow won!", False, black)
 
 while True:
     for event in pygame.event.get():
@@ -132,13 +138,25 @@ while True:
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             for entry_slot in board.entry_slots:
-                if entry_slot.is_cursor() and Disk(entry_slot.x, 0, red) not in board.disks:
+                if entry_slot.is_cursor() and Disk(entry_slot.x, 0, red) not in board.disks and not board.won:
                     if turn % 2 == 1:
                         board.add_disk(Disk(entry_slot.x, 0, red))
                         turn += 1
                     else:
                         board.add_disk(Disk(entry_slot.x, 0, yellow))
                         turn += 1
+
     board.draw()
+    pygame.draw.rect(screen, grey, bottom_text_panel)
+    if not board.won:
+        if turn % 2 == 1:
+            screen.blit(display_red_turn, (50, 650))
+        else:
+            screen.blit(display_yellow_turn, (50, 650))
+    else:
+        if turn % 2 == 0:
+            screen.blit(display_red_won, (50, 650))
+        else:
+            screen.blit(display_yellow_won, (50, 650))
     pygame.display.flip()
     clock.tick(60)
